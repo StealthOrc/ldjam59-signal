db13aa9206192a411d47204304a9bb9e29f1ab72
Shiirroo <merken@hotmail.de>
2026-04-20 23:11:21 +0200

    Fix marketplace descriptor UI test stub

--- tests/ui_formatting_test.lua
+++ tests/ui_formatting_test.lua
@@ -390,6 +390,9 @@ local marketplaceDescriptorGame = {
     levelSelectMode = "marketplace",
     levelSelectMarketplaceTab = "top",
     levelSelectMarketplaceSearchQuery = "",
+    getMarketplaceFavoriteAnimation = function()
+        return nil
+    end,
     getMarketplaceEntries = function()
         return {
             {
