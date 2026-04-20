62b68b8d8b99c48cd74893de6c99ae640d8c5a24
TatsuDev <nitram1999@hotmail.de>
2026-04-20 23:38:40 +0200

    Reorder level select and rename guidebook (#65)
    * Rename campaign maps to enforce display order
    
    * Reorder level select and rename guidebook
    
    * Reorder level select tabs and default to campaign

--- src/game/game.lua
+++ src/game/game.lua
@@ -379,7 +379,7 @@ function Game.new()
     self.levelSelectIssue = nil
     self.levelSelectSelectedId = nil
     self.levelSelectSelectedMapUuid = nil
-    self.levelSelectFilter = "all"
+    self.levelSelectFilter = "campaign"
     self.levelSelectHoverId = nil
     self.levelSelectHoverInfo = nil
     self.levelSelectVisualIndex = nil
@@ -2752,7 +2752,7 @@ function Game:resetLevelSelectVisualIndex()
 end
 
 function Game:setLevelSelectFilter(filterId)
-    self.levelSelectFilter = filterId or "all"
+    self.levelSelectFilter = filterId or "campaign"
     self:clearLevelSelectActionState()
     self:getSelectedLevelMap()
     self:resetLevelSelectVisualIndex()
@@ -3006,7 +3006,7 @@ end
 function Game:openLevelSelect()
     self.screen = "level_select"
     self.levelSelectIssue = nil
-    self.levelSelectFilter = "all"
+    self.levelSelectFilter = "campaign"
     self.levelSelectHoverId = nil
     self.levelSelectHoverInfo = nil
     self.levelSelectMode = LEVEL_SELECT_MODE_LIBRARY
